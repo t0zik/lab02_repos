ae519531785e965fea6a5a1490f6ac34a34778f0
t0zik <t0zarsad@gmail.com>
2022-11-08 18:35:24 +0200

    first main commit

---
+++ PR2/Source.cpp
@@ -0,0 +1,23 @@
+// Lab_02.cpp
+// < Товарницький Назарій Володимировч >
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 16
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double Pi = 4 * atan(1.); // число пі
+	double x; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "x = "; cin >> x;
+	z1 = (x * x + 2 * x - 3 + (x + 1) * sqrt(x * x - 9)) / (x * x - 2 * x - 3 + (x - 1) * sqrt(x * x - 9));
+	z2 = sqrt((x + 3) / (x - 3));
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
